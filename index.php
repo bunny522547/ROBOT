8e74033435d87db54b9adf5eff880762817d01b3
bunny522547 <54652708+bunny522547@users.noreply.github.com>
2019-11-18 08:30:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -56,6 +56,12 @@
         $arrayPostData['messages'][0]['text'] = "ราคาปุ๋ยยางพาราที่ถูกที่สุดในช่งอายุต่าง ๆ"."\n"."https://docs.google.com/document/d/1xnbQIHYP_yboKn3CEE819JvgdpLwhJVhgp2aACyc-ww/edit";
         replyMsg($arrayHeader,$arrayPostData);
     }
+   /*else if($message == "6"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "วิธีการผสมปุ๋ยใช้เอง"."\n"."https://docs.google.com/document/d/1xnbQIHYP_yboKn3CEE819JvgdpLwhJVhgp2aACyc-ww/edit";
+        replyMsg($arrayHeader,$arrayPostData);
+    }*/
     else if($message == "ฝันดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "sticker";
